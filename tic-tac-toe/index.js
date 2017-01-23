a3ed0d3fb002311a7aeb25c53a632c058d12c5f3
Anton <anton.arboleda2gmail.com>
2017-01-23 15:31:37 -0800

    Add some game logic. Create boxes on to screen

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,3 +1,25 @@
 $(function () {
-  // $()
+
+  // things we know for jquery
+
+  // player one function 
+
+  //player two function
+
+  // if player ones turn append x to the box
+  $('.square').on('click', function () {
+    this.append('X');  
+  });
+
+  // if player two's turn, append an O to the box
+
+  // $('.square').on('click', function () {
+  //   this.append('O');
+  // });
+  
+
+
+
+
+
 });
